115d403e841c56ce9d59d0ac2347eef1a7a18192
bajajra30 <rahul.bajaj2016@vitstudent.ac.in>
2018-03-13 21:56:59 +0530

    correction 15

--- foo.c
+++ foo.c
@@ -29,7 +29,7 @@ main(int argc, char *argv[])
       break;
     }
   }
-  for ( z = 0; z < 3000000000000000000.0; z += 0.01 )
+  for ( z = 0; z < 30.0; z += 0.01 )
        x =  x + 3.14 * 89.64;   // useless calculations to consume CPU time
   exit();
 }
